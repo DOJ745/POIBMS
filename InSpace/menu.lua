d143d6ad91add0d5288380600ac98bc5e4ef6472
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-09-07 15:22:48 +0300

    Fixed scene uploading

---
+++ InSpace/menu.lua
@@ -0,0 +1,54 @@
+local composer = require( "composer" )
+ 
+local scene = composer.newScene()
+
+local function gotoGame()
+    composer.gotoScene( "game" )
+end
+ 
+local function gotoHighScores()
+    composer.gotoScene( "highscores" )
+end
+
+function scene:create( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs when the scene is first created but has not yet appeared on screen
+ 
+    local background = display.newImageRect( sceneGroup, "images/background.jpg", 900, 1900 )
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
+
+    local title = display.newImageRect( sceneGroup, "images/title.png", 350, 75 )
+    title.x = display.contentCenterX
+    title.y = 200
+
+    local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, 700, native.systemFont, 44 )
+    playButton:setFillColor( 0.82, 0.86, 1 )
+ 
+    local highScoresButton = display.newText( sceneGroup, "High Scores", display.contentCenterX, 810, native.systemFont, 44 )
+    highScoresButton:setFillColor( 0.75, 0.78, 1 )
+
+    playButton:addEventListener( "tap", gotoGame )
+    highScoresButton:addEventListener( "tap", gotoHighScores )
+
+end
+
+function scene:enter( event )
+
+end
+
+function scene:exit( event )
+
+end
+
+function scene:destroy( event )
+
+end
+
+scene:addEventListener( "create", scene )
+scene:addEventListener( "enter", scene )
+scene:addEventListener( "exit", scene )
+scene:addEventListener( "destroy", scene )
+
+return scene
